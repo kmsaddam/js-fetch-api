7ddc96b71468073f3891e8d5f3b62d284201e90c
K M Saddam Hossain <dev.saddam@gmail.com>
2022-11-16 13:41:29 +0600

    Code Clean

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 document.querySelector("#get_data").addEventListener('click', getData);
 function getData(e) {
-    // console.log(`This is test data`);
-
     fetch('https://api.chucknos.io/jokes/search?query=drama')
-    .then(res => {
-        return res.json();
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(err => {
-        console.log(err.message);
-    })
+    .then(res =>  res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err.message));
 
     e.preventDefault();
 }

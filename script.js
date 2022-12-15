a5f7c3c9f7ccfdec2cd45886707248c31fbd31c0
K M Saddam Hossain <dev.saddam@gmail.com>
2022-12-15 13:11:26 +0600

    Code Organize

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-document.querySelector("#get_data").addEventListener('click', getData);
-function getData(e) {
-    fetch('https://api.chucknos.io/jokes/search?query=drama')
-    .then(res =>  res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err.message));
+document.querySelector("#get_data").addEventListener("click", getData);
 
-    e.preventDefault();
+function getData(e) {
+  fetch(`https://api.chucknorris.io/jokes/search?query=Chuck Norris`)
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err.message));
+
+  e.preventDefault();
 }
